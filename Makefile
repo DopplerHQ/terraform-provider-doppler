DopplerHQ/terraform-provider-doppler

commit 0bfb89a59027e21abf78ed7c92ae0348214493cc

Update Makefile to build with correct arch for local development
Related to Intel vs Apple Chip Macs.

Makefile
@@ -5,7 +5,7 @@ NAME=doppler
 BINARY=terraform-provider-${NAME}
 # Only used for local development
 VERSION=0.0.1
-OS_ARCH=darwin_amd64
+OS_ARCH=darwin_$$(uname -m)
 
 default: install
 

commit fb6e43f01bdaae01cd8f5680b08ff1dba06a66f5

Fix OS_ARCH for x86_64
This bug was introduced in 0bfb89a and is caused by Terraform always identifying
x86_64 arches as amd64.

Makefile
@@ -5,7 +5,16 @@ NAME=doppler
 BINARY=terraform-provider-${NAME}
 # Only used for local development
 VERSION=0.0.1
-OS_ARCH=darwin_$$(uname -m)
+ARCH := $(shell uname -m)
+# required because Terraform always evaluates x86_64
+# architectures as amd64
+ifeq ($(ARCH), x86_64)
+  OS_ARCH=darwin_amd64
+else ifeq ($(ARCH), arm64)
+  OS_ARCH=darwin_arm64
+else
+  $(error $(ARCH) is currently untested. Please update the Makefile to handle your architecture properly.)
+endif
 
 default: install
 