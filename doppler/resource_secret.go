DopplerHQ/terraform-provider-doppler

commit b7c7c6a91dedd2345b73eb42aaeacc90879a7afb

Fix issue where old secret would not be deleted during renames
If a secret is renamed, we need to delete the secret with the old name.

This commit additionally removes an unnecessary value check which prevented setting secrets with an empty string value, which is valid.

doppler/resource_secret.go
@@ -58,11 +58,15 @@ func resourceSecretUpdate(ctx context.Context, d *schema.ResourceData, m interfa
 	config := d.Get("config").(string)
 	name := d.Get("name").(string)
 	value := d.Get("value").(string)
-	if value != "" {
-		newSecret := RawSecret{Name: name, Value: &value}
-		if err := client.UpdateSecrets(ctx, project, config, []RawSecret{newSecret}); err != nil {
-			return diag.FromErr(err)
-		}
+
+	secrets := []RawSecret{{Name: name, Value: &value}}
+	if d.HasChange("name") {
+		previousName, _ := d.GetChange("name")
+		secrets = append(secrets, RawSecret{Name: previousName.(string), Value: nil})
+	}
+
+	if err := client.UpdateSecrets(ctx, project, config, secrets); err != nil {
+		return diag.FromErr(err)
 	}
 
 	d.SetId(getSecretId(project, config, name))

commit 01987e01072b8f159d2f2d85b816797ab5a8de73

Fix issue preventing creation of new secret resources
We now only attempt deleting the old name if the resource is not being created.

Fixes regression introduced by b7c7c6a91dedd2345b73eb42aaeacc90879a7afb

doppler/resource_secret.go
@@ -60,7 +60,7 @@ func resourceSecretUpdate(ctx context.Context, d *schema.ResourceData, m interfa
 	value := d.Get("value").(string)
 
 	secrets := []RawSecret{{Name: name, Value: &value}}
-	if d.HasChange("name") {
+	if !d.IsNewResource() && d.HasChange("name") {
 		previousName, _ := d.GetChange("name")
 		secrets = append(secrets, RawSecret{Name: previousName.(string), Value: nil})
 	}