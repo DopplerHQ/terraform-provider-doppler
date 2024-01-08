ffefabdf7047f1b97a39c6cc3c4ef90c6d73d80f
Steve Brudz <sbrudz@defmethod.io>
2024-01-08 09:11:36 -0600

    Add resource sync type for Terraform Cloud

--- doppler/resource_sync_types.go
+++ doppler/resource_sync_types.go
@@ -73,3 +73,50 @@ func resourceSyncAWSParameterStore() *schema.Resource {
 	}
 	return builder.Build()
 }
+
+func resourceSyncTerraformCloud() *schema.Resource {
+	builder := ResourceSyncBuilder{
+		DataSchema: map[string]*schema.Schema{
+			"sync_target": {
+				Description: "Either \"workplace\" or \"variableSet\", based on the resource type to sync to",
+				Type:        schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
+			},
+			"workplace_id": {
+				Description: "The Terraform Cloud workplace ID to sync to",
+				Type:        schema.TypeString,
+				Optional:    true,
+				ForceNew:    true,
+			},
+			"variable_set_id": {
+				Description: "The Terraform Cloud variable set ID to sync to",
+				Type:        schema.TypeString,
+				Optional:    true,
+				ForceNew:    true,
+			},
+			"variable_sync_type": {
+				Description: "Either \"terraform\" to sync secrets as Terraform variables or \"env\" to sync as environment variables",
+				Type:        schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
+			},
+			"name_transform": {
+				Description: "A name transform to apply before syncing secrets: \"none\" or \"lowercase\"",
+				Type:        schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
+			},
+		},
+		DataBuilder: func(d *schema.ResourceData) IntegrationData {
+			return map[string]interface{}{
+				"sync_target":        d.Get("sync_target"),
+				"workplace_id":       d.Get("workplace_id"),
+				"variable_set_id":    d.Get("variable_set_id"),
+				"variable_sync_type": d.Get("variable_sync_type"),
+				"name_transform":     d.Get("name_transform"),
+			}
+		},
+	}
+	return builder.Build()
+}
