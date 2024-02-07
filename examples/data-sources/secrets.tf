27c701ea9b131003dbc3de6bdb269ab51c235883
Nic Manoogian <nic.manoogian@doppler.com>
2024-02-07 10:52:07 -0500

    Fix secrets data source docs and regenerate

--- examples/data-sources/secrets.tf
+++ examples/data-sources/secrets.tf
@@ -1,3 +1,5 @@
+### Basic Usage
+
 data "doppler_secrets" "this" {}
 
 # Access individual secrets
@@ -15,4 +17,42 @@ output "max_workers" {
 # e.g. FEATURE_FLAGS = `{ "AUTOPILOT": true, "TOP_SPEED": 130 }`
 output "json_parsing_values" {
   value = nonsensitive(jsondecode(data.doppler_secrets.this.map.FEATURE_FLAGS)["TOP_SPEED"])
+}
+
+### Referencing secrets from multiple projects
+
+variable "doppler_token_dev" {
+  type = string
+  description = "A token to authenticate with Doppler for the dev config"
+}
+
+variable "doppler_token_prd" {
+  type = string
+  description = "A token to authenticate with Doppler for the prd config"
+}
+
+provider "doppler" {
+  doppler_token = var.doppler_token_dev
+  alias = "dev"
+}
+
+provider "doppler" {
+  doppler_token = var.doppler_token_prd
+  alias = "prd"
+}
+
+data "doppler_secrets" "dev" {
+  provider = doppler.dev
+}
+
+data "doppler_secrets" "prd" {
+  provider = doppler.prd
+}
+
+output "port-dev" {
+  value = nonsensitive(data.doppler_secrets.dev.map.PORT)
+}
+
+output "port-prd" {
+  value = nonsensitive(data.doppler_secrets.prd.map.PORT)
 }
