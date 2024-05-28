7fc741c7c738f769ad1d8590f2b9289eed7fe498
Kyle McGuire <kyle.mcguire@doppler.com>
2024-05-28 11:57:33 -0700

    Add Webhook example and md template

---
+++ examples/resources/webhook.tf
@@ -0,0 +1,14 @@
+resource "doppler_webhook" "ci" {
+  project = doppler_project.test_proj.name
+  url = "https://localhost/webhook"
+  secret = "my signing secret-2"
+  enabled = true
+  enabled_configs = [doppler_config.ci_github.name]
+  authentication {
+    type = "Bearer"
+    token = "my bearer token"
+  }
+  payload = jsonencode({
+    myKey = "my value"
+  })
+}
