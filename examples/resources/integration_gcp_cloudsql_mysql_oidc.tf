02c7ddbd042d12f47615d0ec1298f3dd7167a9b6
yijiao.wang <yijiao.wang@doppler.com>
2026-08-17 02:32:47 -0700

    Add support for GCP and Azure OIDC with examples

---
+++ examples/resources/integration_gcp_cloudsql_mysql_oidc.tf
@@ -0,0 +1,11 @@
+resource "doppler_integration_gcp_cloudsql_mysql_oidc" "prod" {
+  name                           = "Production"
+  gcp_workload_identity_provider = "//iam.googleapis.com/projects/123456789/locations/global/workloadIdentityPools/doppler-pool/providers/doppler"
+  gcp_project_id                 = "your-gcp-project-id"
+}
+
+resource "google_project_iam_member" "doppler" {
+  project = "your-gcp-project-id"
+  role    = "roles/cloudsql.admin"
+  member  = doppler_integration_gcp_cloudsql_mysql_oidc.prod.federation_principal
+}
